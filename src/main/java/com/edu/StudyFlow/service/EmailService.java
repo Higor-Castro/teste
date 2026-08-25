2d37b3b7a426aaeb27b88af4b1897b44fc2b5549
Higor Castro <higorcastro142@gmail.com>
2026-08-25 07:48:38 -0300

    email teste

--- src/main/java/com/edu/StudyFlow/service/EmailService.java
+++ src/main/java/com/edu/StudyFlow/service/EmailService.java
@@ -1,5 +1,6 @@
 package com.edu.StudyFlow.service;
 
+import com.edu.StudyFlow.exception.RequisicaoInvalidaException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
@@ -14,19 +15,21 @@ public class EmailService {
     @Value("${brevo.api-key}")
     private String apiKey;
 
+
     @Value("${brevo.email}")
     private String emailRemetente;
 
     private final RestTemplate restTemplate = new RestTemplate();
 
     public void enviarEmail(String destinatario, String corpo) {
+        System.out.println("API KEY sendo usada: " + apiKey);
+        System.out.println("emailRemetente: " + emailRemetente);
 
         String url = "https://api.brevo.com/v3/smtp/email";
 
         HttpHeaders headers = new HttpHeaders();
-
         headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.set("api-key", apiKey);
+        headers.set("api-key", apiKey.trim());
 
         Map<String, Object> remetente = new HashMap<>();
         remetente.put("email", emailRemetente);
@@ -37,17 +40,16 @@ public class EmailService {
 
         Map<String, Object> body = new HashMap<>();
         body.put("sender", remetente);
-        body.put("to", new Map[]{destinatarioMap});
+        body.put("to", java.util.List.of(destinatarioMap));
         body.put("subject", "StudyFlow");
         body.put("textContent", corpo);
 
-        HttpEntity<Map<String, Object>> request =
-                new HttpEntity<>(body, headers);
+        HttpEntity<Map<String, Object>> request = new HttpEntity<>(body, headers);
 
-        restTemplate.postForEntity(
-                url,
-                request,
-                String.class
-        );
+        try {
+            restTemplate.postForEntity(url, request, String.class);
+        } catch (Exception e) {
+            throw new RequisicaoInvalidaException("Falha ao enviar email: " + e.getMessage());
+        }
     }
 }
